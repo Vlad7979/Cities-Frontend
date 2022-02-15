e458d695258f3ca894873408b5e667a8d151f93b
Vlad7979 <vlad3005867@gmail.com>
2022-02-15 19:00:43 +0300

    feat(project): add Dockerfile to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Switch, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -14,87 +14,87 @@ import Edit from "./components/edit.component";
 import EventBus from "./common/EventBus";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.logOut = this.logOut.bind(this);
+    constructor(props) {
+        super(props);
+        this.logOut = this.logOut.bind(this);
 
-    this.state = {
-      currentUser: undefined
-    };
-  }
-
-  componentDidMount() {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      this.setState({
-        currentUser: user
-      });
+        this.state = {
+            currentUser: undefined
+        };
     }
-    
-    EventBus.on("logout", () => {
-      this.logOut();
-    });
-  }
 
-  componentWillUnmount() {
-    EventBus.remove("logout");
-  }
+    componentDidMount() {
+        const user = AuthService.getCurrentUser();
 
-  logOut() {
-    AuthService.logout();
-    this.setState({
-      currentUser: undefined
-    });
-  }
+        if (user) {
+            this.setState({
+                currentUser: user
+            });
+        }
 
-  render() {
-    const { currentUser } = this.state;
+        EventBus.on("logout", () => {
+            this.logOut();
+        });
+    }
 
-    return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            Cities
-          </Link>
+    componentWillUnmount() {
+        EventBus.remove("logout");
+    }
 
-          {currentUser ? (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
-                  Logout
-                </a>
-              </li>
+    logOut() {
+        AuthService.logout();
+        this.setState({
+            currentUser: undefined
+        });
+    }
+
+    render() {
+        const {currentUser} = this.state;
+
+        return (
+            <div>
+                <nav className="navbar navbar-expand navbar-dark bg-dark">
+                    <Link to={"/"} className="navbar-brand">
+                        Cities
+                    </Link>
+
+                    {currentUser ? (
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a href="/login" className="nav-link" onClick={this.logOut}>
+                                    Logout
+                                </a>
+                            </li>
+                        </div>
+                    ) : (
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <Link to={"/login"} className="nav-link">
+                                    Login
+                                </Link>
+                            </li>
+
+                            <li className="nav-item">
+                                <Link to={"/register"} className="nav-link">
+                                    Sign Up
+                                </Link>
+                            </li>
+                        </div>
+                    )}
+                </nav>
+
+                <div className="container mt-3">
+                    <Switch>
+                        <Route exact path={["/", "/home"]} component={Home}/>
+                        <Route exact path="/city" component={City}/>
+                        <Route exact path="/edit" component={Edit}/>
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/register" component={Register}/>
+                    </Switch>
+                </div>
             </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Sign Up
-                </Link>
-              </li>
-            </div>
-          )}
-        </nav>
-
-        <div className="container mt-3">
-          <Switch>
-            <Route exact path={["/", "/home"]} component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
-            <Route path="/city" component={City} />
-            <Route path="/edit" component={Edit} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/services/city.service.js
+++ src/services/city.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/city/';
+const API_URL = 'http://localhost:8081/api/city/';
 
 class CityService {
 
